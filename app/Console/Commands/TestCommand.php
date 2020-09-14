688b03271ba90ae903afb9b757a0df04a82d8051
izuru-t <mana_land@hotmail.com>
2020-09-14 15:18:25 +0000

    update test_command

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -1,5 +1,47 @@
-vi .env
-DB_DATABASE=worldcup2014db // データベース名を変更
+<?php
 
-php artisan .clear
-php artisan test_command // 選手の名前がひょうじされればOKです
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Player;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test_command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Test Command';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $players = Player::get();
+        foreach($players as $player) {
+            echo $player->name."\n";
+        }
+        return 0;
+    }
+}
